768cfd4240611258626750bfc8c2a238b4e0381f
Matti Tuomikoski <mtuomikoski@gmail.com>
2021-06-16 13:01:21 +0300

    Run context load test in test profile

--- src/test/java/com/mtuomiko/springtodobackend/SpringTodoBackendApplicationTests.java
+++ src/test/java/com/mtuomiko/springtodobackend/SpringTodoBackendApplicationTests.java
@@ -2,7 +2,9 @@ package com.mtuomiko.springtodobackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class SpringTodoBackendApplicationTests {
 
